3053c74141929215e74605327379563bacf8c180
ewen575724 <ewen575724@gmail.com>
2026-07-08 19:29:48 +0200

    fix Canonical Changes

--- crates/canonical-agda/src/lib.rs
+++ crates/canonical-agda/src/lib.rs
@@ -1,11 +1,9 @@
 use canonical_compat::ir::*;
 use canonical_core::core::*;
-use canonical_core::memory::S;
 use canonical_core::prover::*;
 use canonical_core::search::*;
 use serde::{Deserialize, Serialize};
 use serde_json::from_str;
-use snailquote::unescape;
 use std::ops::Deref;
 use std::sync::atomic::Ordering;
 use std::sync::mpsc::{self, Sender};
@@ -22,178 +20,153 @@ use std::io::Write;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct HSpine {
-    shead: String,
-    sargs: Vec<HTerm>,
+    head: String,
+    args: Vec<HExpr>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct HTerm {
-    thead: Vec<String>,
-    targs: HSpine,
+struct HEquation{
+    lhs : HSpine,
+    rhs : HSpine
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct HDecl {
+    name: String,
+    typ: Option<HExpr>,
+    equations : Vec<HEquation>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct HRule {
-    rlhs : HSpine,
-    rrhs : HSpine
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct HType {
-    bindings: Vec<(String, Option <HType>)>,
-    lets: Vec<(String, Option<HType>, Vec<HRule>)>,
-    codom: HSpine,
-}
-
-fn to_ir_var(v: &String) -> IRVar {
-    IRVar { name: v.to_owned() }
-}
-
-fn var_to_string(v: &IRVar) -> String {
-    v.name.to_string()
-}
-
-fn to_ir_term(te: &HTerm) -> IRTerm {
-    IRTerm {
-        params: te.thead.iter().map(to_ir_var).collect(),
-        lets: Vec::new(),
-        spine: to_ir_spine(&te.targs),
-        goal_rules: Vec::new(),
-    }
-}
-
-fn to_hterm(te: &IRTerm) -> HTerm {
-    HTerm {
-        thead: te.params.iter().map(var_to_string).collect(),
-        targs: to_hspine(&te.spine),
-    }
+struct HExpr {
+    params: Vec<HDecl>,
+    lets: Vec<HDecl>,
+    spine: HSpine,
 }
 
 fn to_ir_spine(sp: &HSpine) -> IRSpine {
     IRSpine {
-        head: sp.shead.to_owned(),
-        args: sp.sargs.iter().map(to_ir_term).collect(),
+        head: sp.head.to_owned(),
+        args: sp.args.iter().map(to_ir_expr).collect(),
         premise_rules: Vec::new(),
     }
 }
 
-fn to_hspine(sp: &IRSpine) -> HSpine {
-    HSpine {
-        shead: sp.head.to_string(),
-        sargs: sp.args.iter().map(to_hterm).collect(),
+fn to_ir_expr(e: &HExpr) -> IRExpr {
+    IRExpr {
+        params : e.params.iter().map(to_ir_decl).collect(),
+        lets : e.lets.iter().map(to_ir_decl).collect(),
+        spine : to_ir_spine(&e.spine),
+        goal_rules : Vec::new()
     }
 }
 
-fn to_ir_rule(rl : &HRule) -> IRRule {
-    IRRule{
-        lhs: to_ir_spine(&rl.rlhs),
-        rhs: to_ir_spine(&rl.rrhs),
-        attribution : Vec::new(),
+fn to_ir_decl(d: &HDecl) -> IRDecl {
+    IRDecl {
+        name : d.name.to_owned(),
+        typ  : match &d.typ {
+            None => None,
+            Some(d) => Some (to_ir_expr(&d))
+        },
+        equations : Vec::new()
+    }
+}
+
+fn to_ir_equation(e : &HEquation) -> IREquation {
+    IREquation {
+        lhs : to_ir_spine(&e.lhs),
+        rhs : to_ir_spine(&e.rhs),
+        attribution : Vec::new()   ,
         is_redex : false
     }
 }
 
-fn to_ir_type(ty: &HType) -> IRType {
-    IRType {
-        params: ty
-            .bindings
-            .iter()
-            .map(|(_, t)| match t { None => None,  Some(ty) => Some(to_ir_type(ty.to_owned())),})
-            .collect(),
-        lets: ty
-            .lets
-            .iter()
-            .map(|(_, t, _ )| match t { None => None,  Some(ty) => Some(to_ir_type(ty.to_owned())),})
-            .collect(),
-        codomain: IRTerm {
-            params: ty
-                .bindings
-                .iter()
-                .map(|(s, _)| IRVar { name: s.to_owned() })
-                .collect(),
-            lets: ty
-                .lets
-                .iter()
-                .map(|(s, _, rs)| IRLet {
-                    var: IRVar { name: s.to_owned() },
-                    rules: rs.iter().map(|r| to_ir_rule(r)).collect() }
-                )
-                .collect(),
-            spine: to_ir_spine(&ty.codom),
-            goal_rules: Vec::new(),
+fn to_hdecl(d : &IRDecl) -> HDecl {
+    HDecl {
+        name : d.name.to_owned(),
+        typ : match &d.typ {
+          None => None,
+          Some(e) => Some(to_hexpr(&e))
         },
+        equations : d.equations.iter().map(to_hequation).collect()
     }
 }
 
-fn main(
-    prover: Prover,
-    sender: Sender<()>,
-    count: usize,
-    terms: Arc<Mutex<Vec<IRTerm>>>,
-) -> (DFSResult, u32) {
-    prover.prove(
-        &|term: Term| {
-            let mut v = terms.lock().unwrap();
-            let bindings = term
-                .base
-                .borrow()
-                .gamma
-                .linked
-                .as_ref()
-                .unwrap()
-                .borrow()
-                .node
-                .bindings
-                .clone();
-            let ir_term = IRTerm::from_lambda::<false>(term, bindings, false);
-            if v.len() < count && v.iter().all(|x| x != &ir_term) {
-                v.push(ir_term);
-            }
-            if v.len() >= count {
-                RUN.store(false, Ordering::Relaxed);
-                sender.send(()).unwrap();
-            }
-        },
-        false,
-    )
+fn to_hspine(s : &IRSpine) -> HSpine {
+    HSpine {
+        head : s.head.to_owned(),
+        args : s.args.iter().map(to_hexpr).collect()
+    }
+}
+
+fn to_hequation(e : &IREquation) -> HEquation {
+    HEquation{
+        lhs : to_hspine(&e.lhs),
+        rhs : to_hspine(&e.rhs)
+    }
+}
+
+fn to_hexpr(e : &IRExpr) -> HExpr {
+    HExpr {
+        params : e.params.iter().map(to_hdecl).collect(),
+        lets : e.lets.iter().map(to_hdecl).collect(),
+        spine : to_hspine(&e.spine)
+
+    }
+}
+
+fn main(prover: Prover, sender: Sender<()>, count: usize, terms: Arc<Mutex<Vec<IRExpr>>>) -> (DFSResult, u32) {
+    prover.prove(&|term: Term| {
+        let mut v = terms.lock().unwrap();
+        let bindings = term.base.borrow().gamma.linked.as_ref().unwrap().borrow().node.bindings.clone();
+        let ir_term = IRExpr::from_lambda::<false>(term, bindings, false);
+        if v.len() < count && v.iter().all(|x| x != &ir_term) {
+            v.push(ir_term);
+        }
+        if v.len() >= count {
+            RUN.store(false, Ordering::Relaxed);
+            sender.send(()).unwrap();
+        }
+    }, false)
 }
 
 
 #[no_mangle]
 pub extern "C" fn canonical(
-    typ: *const c_char,
+    goal: *const c_char,
     name: *const c_char,
     timeout: u64,
     count: usize,
 ) -> *mut c_char {
     unsafe {
         // // get the type transmitted by haskell
-        let cstr = CStr::from_ptr(typ).to_str().unwrap();
+        let cstr = CStr::from_ptr(goal).to_str().unwrap();
         // let rstr = unescape(cstr.to_str().unwrap()).unwrap();
-        let typ: HType =
+        let goal: HDecl =
             serde_json::from_str(cstr).expect("Failed to convert the JSON to a type.\n");
 
-        // get the IRType
-        let ir_type = to_ir_type(&typ);
+        // get the IRDecl
+        let ir_decl = to_ir_decl(&goal);
 
         // Magic
         let (tx, rx) = mpsc::channel();
 
-        let arc: Arc<Mutex<Vec<IRTerm>>> = Arc::new(Mutex::new(Vec::new()));
+
+
+        let arc : Arc<Mutex<Vec<IRExpr>>> = Arc::new(Mutex::new(Vec::new()));
         let arc_clone = arc.clone();
-        let tb = S::new(ir_type.to_type(&ES::new()));
-        let problem_bind = S::new(Bind::new(
-            CStr::from_ptr(name).to_string_lossy().into_owned(),
-        ));
         let mut owned_linked = Vec::new();
-        let prover = Prover::new(tb.downgrade(), problem_bind.downgrade(), &mut owned_linked);
-        let worker = thread::spawn(move || main(prover, tx, count, arc_clone));
+        let problem = ir_decl.to_problem(&mut owned_linked);
+        let prover = Prover::new(problem.downgrade());
+
+        let worker = thread::spawn(move || {
+            main(prover, tx, count, arc_clone)
+        });
         let _ = rx.recv_timeout(Duration::from_secs(timeout));
         RUN.store(false, Ordering::Relaxed);
-        let res: Vec<HTerm> = match worker.join() {
+        let res: Vec<HExpr> = match worker.join() {
             Ok((_, _)) => {
                 let v = arc.lock().unwrap();
-                v.deref().iter().map(to_hterm).collect()
+                v.deref().iter().map(to_hexpr).collect()
             }
             Err(e) => {
                 let msg = if let Some(s) = e.downcast_ref::<String>() {
